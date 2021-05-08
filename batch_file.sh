04111fe2712e0b42d0668707d46cd36406b531be
Ashkan Mirzaee <ashkan.m23@gmail.com>
2021-05-07 19:24:16 -0500

    Update if clauses

--- batch_file.sh
+++ batch_file.sh
@@ -18,14 +18,20 @@ echo ================ HTML queries =============== $(hostname) $(date)
 ## Download level of forest attributes from online source (EVALIDator)
 if jq ."query_type" config.json | grep -q "state"; then
     python3 fia_state.py config.json attributes.json
-elif jq ."query_type" config.json | grep -q "county"; then
+fi
+
+if jq ."query_type" config.json | grep -q "county"; then
     python3 fia_county.py config.json attributes.json
-elif jq ."query_type" config.json | grep -q "coordinate"; then
+fi
+
+if jq ."query_type" config.json | grep -q "coordinate"; then
     if [ -f coordinates.json ]; then
 	python3 fia_coordinate.py config.json attributes.json coordinates.json
     else
 	"Please add place coordinates in this directrory. Acceptable file format is CSV and the file name should be 'coordinates.csv'"
     fi
-else
+fi
+
+if ! jq ."query_type" config.json | grep -Pq "state|county|coordinate"; then
     echo "Please select one of the available methods for 'query_type'. Available methods are 'state', 'county', 'coordinate'"
 fi
