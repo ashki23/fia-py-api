b35e857fbcaeeab7926f945c035dcb981e19d36a
Ashkan Mirzaee <ashkan.m23@gmail.com>
2021-05-08 17:04:29 -0500

    Download state census info

--- download.sh
+++ download.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 wget -c -nv --tries=2 https://apps.fs.usda.gov/fia/datamart/CSV/REF_POP_ATTRIBUTE.csv -O ./attributes_all.csv
-wget -c -nv --tries=2 https://gitlab.com/ashki23/rc-biomass/-/raw/main/state_codes.csv
+wget -c -nv --tries=2 https://www2.census.gov/geo/docs/reference/state.txt
 wget -c -nv --tries=2 https://gitlab.com/ashki23/rc-biomass/-/raw/main/neighbor_state.csv
+
+cat state.txt | awk -F '|' '{print $3","$2}' | tail -n +2 > state_abb.csv
+cat state.txt | awk -F '|' '{print $2","$1}' | tail -n +2 > state_codes.csv
