9887a83162131ecf5ff1289f874810d47bfc9f82
Ashkan Mirzaee <ashkan.m23@gmail.com>
2021-05-12 21:17:43 -0500

    Query from other DBs

--- test-db.py
+++ test-db.py
@@ -9,34 +9,74 @@ with open('./config.json') as conf:
     config = json.load(conf)
 year = config['year']
 
-latest = os.popen(f"""
-ls -t ./outputs/county-*.json | head -n 1
-""").read()[:-1]
+q_type = input('Select the query type (insert one of state, county or coordinate): ').lower().strip()
 
-with open(latest) as db:
-    database = json.load(db)
+if q_type == 'state':
+    latest = os.popen(f"""
+    ls -t ./outputs/state-*.json | head -n 1
+    """).read()[:-1]
+    
+    with open(latest) as db:
+        database = json.load(db)
+    
+    ## Input
+    state_in = input('Insert state: ').upper().strip()
+    
+    ## Query
+    state_ = []
+    for db in database:
+        state_.append(db['state'])
+        if db['state'] == state_in:
+                print(db)
+    
+    if state_in not in set(state_):
+        print(f"Sorry! {state_in} is not in the latest database ({latest}).")
 
-## Input
-state_in = input('Insert state: ').upper().strip()
-county_in = input('Insert county: ').capitalize().strip()
+elif q_type == 'county':
+    latest = os.popen(f"""
+    ls -t ./outputs/county-*.json | head -n 1
+    """).read()[:-1]
+    
+    with open(latest) as db:
+        database = json.load(db)
+    
+    ## Input
+    state_in = input('Insert state: ').upper().strip()
+    county_in = input('Insert county: ').capitalize().strip()
+    
+    ## Query
+    state_ = []
+    for db in database:
+        state_.append(db['state'])
+        if db['state'] == state_in:
+            if db['county'] == county_in:
+                print(db)
+    
+    if state_in not in set(state_):
+        print(f"Sorry! {state_in} is not in the latest database ({latest}).")
 
-## Query
-state_ = []
-for db in database:
-    state_.append(db['state'])
-    if db['state'] == state_in:
-        if db['county'] == county_in:
-            print(db)
+elif q_type == 'coordinate':
+    latest = os.popen(f"""
+    ls -t ./outputs/coordinate-*.json | head -n 1
+    """).read()[:-1]
+    
+    with open(latest) as db:
+        database = json.load(db)
+    
+    ## Input
+    lat_in = input('Insert lat: ').strip()
+    lon_in = input('Insert lon: ').strip()
+    
+    ## Query
+    lat_ = []
+    for db in database:
+        lat_.append(db['lat'])
+        if db['lat'] == lat_in:
+            if db['lon'] == lon_in:
+                print(db)
+    
+    if lat_in not in set(lat_):
+        print(f"Sorry! this coordinate is not in the latest database ({latest}).")
 
-if state_in not in set(state_):
-    print(f"Sorry! {state_in} is not in the latest database ({latest}).")
-
-#county_in = open(sys.argv[1]).readline()[:-1].split(',')
-#att_in = open(sys.argv[2]).readline()[:-1].split(',')
-
-#for ct in county_in:
-#    db = database[int(ct)]
-#    for i in att_in:
-#        out = {x:int(y) for x,y in db.items() if x in [f"{int(i)}_{j}" for j in range(min(year), max(year) + 1)]}
-#        print(out)
-#        print(list(out.values()))
+else:
+    print("Please select one of the available methods for 'query_type'. Available methods are 'state', 'county', 'coordinate'.")
