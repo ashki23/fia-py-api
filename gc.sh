1669a81d10f5760f83b5f686c944df67782b8621
Ashkan Mirzaee <ashkan.m23@gmail.com>
2021-06-07 01:37:35 -0500

    Update gc

--- gc.sh
+++ gc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "This is the garbage collector. It will remove job file and collected data in './fia_data' and './job_out*' directories. Do you want to proceed (yes/no)? " answer
+read -p "This is the garbage collector. It will remove job file and collected data in './fia_data' and './job-out*' directories. Do you want to proceed (yes/no)? " answer
 
 if [ $answer = "yes" ]; then
     rm -v ./Miniconda3*
@@ -8,7 +8,7 @@ if [ $answer = "yes" ]; then
     rm -v ./jobid-*
     rm -v ./slurm-*.out
     rm -v ./report-*
-    rm -r ./job_out_* && echo "'/job_out_*' directories were removed"
+    rm -r ./job-out-* && echo "'/job_out_*' directories were removed"
     rm -r ./fia_data && echo "'./fia_data' directory was removed"
 else
     return
