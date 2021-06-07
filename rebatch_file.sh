e6bccc1884aa0a4f72022891466cc75be9e46e80
Ashkan Mirzaee <ashkan.m23@gmail.com>
2021-06-07 01:04:57 -0500

    Update rebatch

--- rebatch_file.sh
+++ rebatch_file.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
 read -p "Select the query type (i.e. state, county or coordinate): " query_name
-if [ $query_name = "state" ]; then query_name="county"; fi
+if [ $query_name = "state" ] || [ $query_name = "county" ]; then query_name="state-county"; echo $query_name
+elif [ $query_name = "coordinate" ]; then echo $query_name
+else echo "Error: Select state, county or coordinate."; return
+fi
 
 ## Set env variables
 source environment.sh
 echo ============ Rebatch time: $(date +"%Y-%m-%d-%H:%M")
 
-## Resubmit the failed jobs
-for j in $(cat ${PROJ_HOME}/job_out_${query_name}/failed.txt); do
-JID=$(sbatch --parseable ${FIA}/job-${j}.sh)
+## Remove invalid json files
+for w in $(cat ${PROJ_HOME}/job-out-${query_name}/warning.txt | grep input | grep -Po "(?<=Warning: ).*(?= is)"); do
+rm $w
+done
+
+## For serial mode
+if [ -z $(which sbatch) ]; then
+. ${FIA}/job-${query_name}-0.sh
+. ${PROJ_HOME}/job-${query_name}.sh
+return
+fi
+
+## Remove the old log file
+rm ${PROJ_HOME}/jobid-${query_name}-rebatch.log
+
+## For parallel: resubmit the failed jobs
+for j in $(cat ${PROJ_HOME}/job-out-${query_name}/failed.txt); do
+JID=$(sbatch --parsable ${FIA}/job-${j}.sh)
 echo ${JID} >> ${PROJ_HOME}/jobid-${query_name}-rebatch.log
 done
 
 ## Re-extract informations from JSON files
 sleep 3
 JOBID=$(cat ${PROJ_HOME}/jobid-${query_name}-rebatch.log | tr '\n' ',' | grep -Po '.*(?=,$)')
-sbatch --parsable --dependency=afterok:$(echo ${JOBID}) ${PROJ_HOME}/job_${query_name}.sh
+sbatch --dependency=afterok:$(echo ${JOBID}) ${PROJ_HOME}/job-${query_name}.sh
